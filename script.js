3b5a8e3f4b157f0cd5f3a7f7c3b23a22a1c1bc81
celdom2020 <7327737@gmail.com>
2022-03-26 00:22:49 +0300

    menu

---
+++ script.js
@@ -0,0 +1,7 @@
+/*let dropphone = document.querySelector(".drop-phone");
+document.querySelector(".fa-mobile").addEventListener("click" , function() {
+    dropphone.style.display = 'block';
+});
+document.querySelector(".fa-times").addEventListener("click" , function() {
+    dropphone.style.display = 'none';
+});*/
